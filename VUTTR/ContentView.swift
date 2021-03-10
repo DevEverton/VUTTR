5b4d59413b0fe48cbd1a7c4b14ec643448ce72dd
Everton Carneiro <everton.mercante@gmail.com>
2021-03-10 16:34:26 -0300

    Initial Commit

---
+++ VUTTR/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VUTTR
+//
+//  Created by Everton Carneiro on 10/03/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ VUTTR/VUTTRApp.swift
@@ -0,0 +1,17 @@
+//
+//  VUTTRApp.swift
+//  VUTTR
+//
+//  Created by Everton Carneiro on 10/03/21.
+//
+
+import SwiftUI
+
+@main
+struct VUTTRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
